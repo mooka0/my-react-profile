4e5ea1c96d4115cc0fefcf4eaea6a08fa87f4c2a
mooka0 <angelankao@gmail.com>
2021-04-15 15:30:08 -0700

    added repo links

--- src/components/Pages/Portfolio.js
+++ src/components/Pages/Portfolio.js
@@ -7,31 +7,49 @@ const Portfolio = () => (
       <div className="grid-item item-1">
         <div className="w3-col l6"><a rel="noreferrer" target="_blank"
           href="https://goals-social-network.herokuapp.com/">Goals Social</a>
+        </div> 
+        <div className="w3-col l6"><a rel="noreferrer" target="_blank"
+          href="https://github.com/Siphon880gh/goals-social">Repository</a>
         </div>
       </div>
       <div className="grid-item item-2">
         <div className="w3-col l6"><a rel="noreferrer" target="_blank"
           href="https://mooka0.github.io/covid19-info-hub/">Covid-19 Info Hub</a>
         </div>
+        <div className="w3-col l6"><a rel="noreferrer" target="_blank"
+          href="https://github.com/mooka0/covid19-info-hub">Repository</a>
+        </div>
       </div>
       <div className="grid-item item-3">
         <div className="w3-col l6"><a rel="noreferrer" target="_blank"
           href="https://book-search-challenge.herokuapp.com/">Book Search</a>
+          </div>
+          <div className="w3-col l6"><a rel="noreferrer" target="_blank"
+          href="https://github.com/mooka0/book-search/">Repository</a>
         </div>
       </div>
       <div className="grid-item item-4">
         <div className="w3-col l6"><a rel="noreferrer" target="_blank"
           href="https://mooka0.github.io/weather-dashboard/">Weather Dashboard</a>
+          </div>
+          <div className="w3-col l6"><a rel="noreferrer" target="_blank"
+          href="https://github.com/mooka0/weather-dashboard">Repository</a>
         </div>
       </div>
       <div className="grid-item item-5">
         <div className="w3-col l6"> <a rel="noreferrer" target="_blank"
           href="https://warm-springs-04629.herokuapp.com/">Movie Friends</a>
+          </div>
+          <div className="w3-col l6"><a rel="noreferrer" target="_blank"
+          href="https://github.com/mooka0/movie-friends">Repository</a>
         </div>
       </div>
       <div className="grid-item item-6">
         <div className="w3-col l6"><a rel="noreferrer" target="_blank"
           href="https://note-taker-project-11.herokuapp.com/">Note Taker</a>
+          </div>
+          <div className="w3-col l6"><a rel="noreferrer" target="_blank"
+          href="https://github.com/mooka0/note-taker">Repository</a>
         </div>
       </div>
     </div>
